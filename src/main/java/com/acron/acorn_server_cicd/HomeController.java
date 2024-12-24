db11541e96ad6daadf8a5b1c48be621d52995558
victoai <victoai999@gmail.com>
2024-12-24 13:22:55 +0900

    dfd

---
+++ src/main/java/com/acron/acorn_server_cicd/HomeController.java
@@ -0,0 +1,14 @@
+package com.acron.acorn_server_cicd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/hi")
+    public  String hi(){
+        return  "hi";
+    }
+}
