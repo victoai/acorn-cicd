5ab77743999ecec7ecb3f570c7fa7a423ef05afb
victoai <victoai999@gmail.com>
2024-12-24 13:13:41 +0900

    project

---
+++ src/main/java/com/acron/acorn_server_cicd/AcornServerCicdApplication.java
@@ -0,0 +1,13 @@
+package com.acron.acorn_server_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcornServerCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcornServerCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acron/acorn_server_cicd/AcornServerCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acron.acorn_server_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcornServerCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
